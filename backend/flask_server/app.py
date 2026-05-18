73e43505e2cceed2d5486957ae69166f3e814714
박희윤 <juya0947@gmail.com>
2026-05-18 15:40:12 +0900

    ㅇ
    ㅇ

--- backend/flask_server/app.py
+++ backend/flask_server/app.py
@@ -18,7 +18,7 @@ db_config = {
      "user": "ADMIN",
      "password": "Heeyoun1220!",
      "dsn": "koreapoint_medium",
-     "wallet_location": "C:/Users/edu__/Downloads/Wallet_Orcle"
+     "wallet_location": "/Users/parkheeyoun/traffic-sync-git/backend/src/main/resources/wallet"
 }
 
 # 3. 모델 입력 컬럼 순서 (학습 시와 동일해야 함)
